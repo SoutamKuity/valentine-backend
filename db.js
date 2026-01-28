bedde8bb9333e35321db3463d2254c86cb3decf9
SoutamKuity <159747230+SoutamKuity@users.noreply.github.com>
2026-01-29 04:18:23 +0530

    Use environment variable for MongoDB connection URI

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://soutam1404:<db_password>@cluster0.wloyi4g..mongodb.net/loveDB?appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("❤️ MongoDB Connected"))
   .catch(err => console.log(err));
